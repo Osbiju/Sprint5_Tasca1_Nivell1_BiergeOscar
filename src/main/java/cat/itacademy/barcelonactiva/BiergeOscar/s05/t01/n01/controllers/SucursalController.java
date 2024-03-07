d3d1394305d1518ca02fac19d8eb3a7f5eb3c93f
Oscar <okeshik@protonmail.com>
2024-03-07 13:48:24 +0100

    Haciendo Controller

--- src/main/java/cat/itacademy/barcelonactiva/BiergeOscar/s05/t01/n01/controllers/SucursalController.java
+++ src/main/java/cat/itacademy/barcelonactiva/BiergeOscar/s05/t01/n01/controllers/SucursalController.java
@@ -1,4 +1,23 @@
 package cat.itacademy.barcelonactiva.BiergeOscar.s05.t01.n01.controllers;
 
+import cat.itacademy.barcelonactiva.BiergeOscar.s05.t01.n01.model.services.SucursalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+@RestController
 public class SucursalController {
+
+    @Autowired
+    private SucursalService sucursalService;
+
+    //para que te devuelva a la pagina de inicio
+    @GetMapping
+    public String homePage(){
+        return "redirect:/sucursal/getAll";
+    }
+    //TODO mirar como utilizar Model model:
+    //https://github.dev/rokysf/Tasca-S5.01/tree/main/S05T01N01MonsIglesiasJordi/src
 }
